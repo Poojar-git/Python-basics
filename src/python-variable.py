347ea666d11215ca667ce07a8c9d5e9014e54491
Poojar-git <pooja.r@diggibyte.com>
2026-06-30 15:09:41 +0530

    Add get the type concept

--- src/python-variable.py
+++ src/python-variable.py
@@ -1,9 +1,17 @@
 print("Variable")
 print("Variable is a name given to the memory location that stores some value")
 
-x=str(2)#Casting
+#Casting is the process of converting a variable from one data type to another data type. In Python, we can cast variables using the following functions:
+x=str(2)
 y=int(4)
 z=float(3)
 print(x)
 print(y)
 print(z)
+
+#Get the Type
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Multi Words Variable Names
