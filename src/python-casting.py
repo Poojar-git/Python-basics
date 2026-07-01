e0c71d8fbfba7c0e387caf1723fd45766e9ec96d
Poojar-git <pooja.r@diggibyte.com>
2026-07-01 10:04:56 +0530

    Fix formatting and add print statements for casting examples

--- src/python-casting.py
+++ src/python-casting.py
@@ -1,4 +1,4 @@
-Casting refers to converting one data type into another. In Python, this can be done using built-in functions like `int()`, `float()`, `str()`, and `bool()`. For example, you can convert a string to an integer using `int("123")`, or convert a float to a string using `str(3.14)`. Casting is useful when you need to perform operations that require specific data types.
+#Casting refers to converting one data type into another. In Python, this can be done using built-in functions like `int()`, `float()`, `str()`, and `bool()`. For example, you can convert a string to an integer using `int("123")`, or convert a float to a string using `str(3.14)`. Casting is useful when you need to perform operations that require specific data types.
 x = int(1)   # x will be 1
 y = int(2.8) # y will be 2
 z = int("3") # z will be 3
@@ -9,3 +9,6 @@ w = float("4.2") # w will be 4.2
 x = str("s1") # x will be 's1'
 y = str(2)    # y will be '2'
 z = str(3.0)  # z will be '3.0'
+print(x)
+print(y)
+print(z)
