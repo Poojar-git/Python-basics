88e5ff60dd6c2e661ff6476c3c1779fd4702e900
Poojar-git <pooja.r@diggibyte.com>
2026-07-01 10:07:10 +0530

    Add initial string handling example and explanation

---
+++ src/python-strings.py
@@ -0,0 +1,4 @@
+#Strings are sequences of characters used to represent text in programming. In Python, strings are enclosed in either single quotes (' ') or double quotes (" "). They can contain letters, numbers, symbols, and whitespace. Strings are immutable, meaning that once they are created, their content cannot be changed. Python provides various methods and operations to manipulate strings, such as concatenation, slicing, and formatting.
+print("Hello")
+#Quotations inside quotes
+print("Hello 'Good Morning'")
