c53ee9a0d1a5a8c8e4f21735a2c92cc042e47506
Poojar-git <pooja.r@diggibyte.com>
2026-07-01 10:08:06 +0530

    Fix typo in comment and add print statement for variable assignment

--- src/python-strings.py
+++ src/python-strings.py
@@ -1,4 +1,8 @@
 #Strings are sequences of characters used to represent text in programming. In Python, strings are enclosed in either single quotes (' ') or double quotes (" "). They can contain letters, numbers, symbols, and whitespace. Strings are immutable, meaning that once they are created, their content cannot be changed. Python provides various methods and operations to manipulate strings, such as concatenation, slicing, and formatting.
 print("Hello")
-#Quotations inside quotes
+#Quotes inside quotes
 print("Hello 'Good Morning'")
+#Assign string to a varaible
+a= "Greeting"
+print(a)
+
