340a976a78df0a79500381174f78864680fc9357
Poojar-git <pooja.r@diggibyte.com>
2026-07-01 10:01:04 +0530

    Add explanation of data type casting in Python

---
+++ src/python-casting.py
@@ -0,0 +1,11 @@
+Casting refers to converting one data type into another. In Python, this can be done using built-in functions like `int()`, `float()`, `str()`, and `bool()`. For example, you can convert a string to an integer using `int("123")`, or convert a float to a string using `str(3.14)`. Casting is useful when you need to perform operations that require specific data types.
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
